166dfdbabc2f0f9bfcedb98c8a85956646fbc8f5
Bryan Muller <71022676+BMuller-dev@users.noreply.github.com>
2022-01-29 22:49:19 +0100

    1

--- shifumi/script.js
+++ shifumi/script.js
@@ -1,16 +1,78 @@
-let rock = document.querySelector('.rock');
-let paper = document.querySelector('.paper');
-let scissor = document.querySelector('.scissor');
-let player_img = document.querySelector('#player_img');
+let player_img = document.querySelector("#player_img");
+let bot_img = document.querySelector("#bot_img");
+let state = document.querySelector('.state');
 
-paper.addEventListener('click', function() {
-    player_img.src = "img/paper.png"
+let restart = document.querySelector('.restart');
+
+restart.addEventListener('click', function() {
+    bot_img.src = "";
+    player_img.src = "";
+    restart.style.visibility = "hidden";
+    state.textContent = "";
+    button.style.visibility = "visible";
+
 })
 
-rock.addEventListener('click', function() {
-    player_img.src = "img/rock.png"
-})
+let player_points = 0;
+let bot_points = 0;
 
-scissor.addEventListener('click', function() {
-    player_img.src = "img/scissor.png"
-})
+let button = document.querySelector(".button");
+button.addEventListener("click", function() {
+    button.style.visibility = "hidden";
+
+    let bot_choice = Math.floor(Math.random() * (4 - 1) + 1);
+
+
+    if (bot_choice == 1) {
+        bot_img.src = "img/rock.png";
+    }
+    if (bot_choice == 2) {
+        bot_img.src = "img/paper.png";
+    }
+    if (bot_choice == 3) {
+        bot_img.src = "img/scissor.png";
+    }
+
+    restart.style.visibility = "visible";
+    if ((player_choice == 1 && bot_choice == 3)(player_choice == 2 && bot_choice == 1)(player_choice == 3 && bot_choice == 2)) {
+        state.textContent = "Victory";
+        player_points++
+    } else {
+        state.textContent = "Defeat";
+        bot_points++
+    }
+    if (player_choice == bot_choice) {
+        state.textContent = "Draw";
+    }
+    points();
+});
+
+let player_choice;
+
+let rock = document.querySelector(".rock");
+rock.addEventListener("click", function() {
+    player_img.src = "img/rock.png";
+    player_choice = 1;
+});
+
+let paper = document.querySelector(".paper");
+paper.addEventListener("click", function() {
+    player_img.src = "img/paper.png";
+    player_choice = 2;
+});
+
+let scissor = document.querySelector(".scissor");
+scissor.addEventListener("click", function() {
+    player_img.src = "img/scissor.png";
+    player_choice = 3;
+});
+
+let player_point = document.querySelector('.player');
+let bot_point = document.querySelector('.bot');
+
+function points() {
+    player_point.textContent = "Player 1 has " + player_points + " points.";
+    bot_point.textContent = "AI has " + player_points + " points."
+
+
+}
